d98b1e66caf24dedeb4040ced1d26ab95e8e9075
pedroargento <peter.argent@gmail.com>
2013-11-11 01:04:50 +1100

    integer problem

--- app/models/buy_action.rb
+++ app/models/buy_action.rb
@@ -2,6 +2,6 @@ class BuyAction < ActiveRecord::Base
 	default_scope order: 'buy_actions.priority ASC'
 	
 	def self.active
-		BuyAction.where('priority > ?', 0)
+		BuyAction.where('priority > ?', '0')
 	end
 end
